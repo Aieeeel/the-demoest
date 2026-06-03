17f0b2eed78a6d1e3fc3c2f27f92f2b283454f52
Aieeeel <aielexalt@gmail.com>
2026-06-04 05:54:57 +0800

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+
+import './globals.css';
+
+export const metadata = {
+  title: 'AIEL',
+  description: 'Accumulate. Invest. Evolve. Legacy.'
+};
+
+export default function RootLayout({children}:{children:React.ReactNode}){
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
